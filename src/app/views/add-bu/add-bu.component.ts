3effa8c482e901c8d5cb581b59bf7d6da62f1eda
Maantu Das <maantudas91@gmail.com>
2018-08-30 13:45:16 +0530

    Update add-bu.component.ts

--- src/app/views/add-bu/add-bu.component.ts
+++ src/app/views/add-bu/add-bu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-add-bu',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-bu.component.css']
 })
 export class AddBuComponent implements OnInit {
+  
+  buForm : FormGroup;
 
-  constructor() { }
+  constructor(private fb : FormBuilder) { }
 
   ngOnInit() {
+    this.initForm();
+  }
+
+  initForm(){
+    this.buForm = this.fb.group({
+      name : new FormControl('', [Validators.required]),
+      reg_no : new FormControl('', [Validators.required]),
+      vehicle_type : new FormControl('', [Validators.required]),
+      address : new FormGroup({
+        state : new FormControl('',[Validators.required]),
+        country : new FormControl('',[Validators.required]),
+        city : new FormControl('',[Validators.required])
+      })
+    });
+  }
+
+
+
+  saveBU(form):void{
+    console.log(form.value)
   }
 
 }
